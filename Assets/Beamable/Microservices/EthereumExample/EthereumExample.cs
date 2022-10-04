9c87c46461f6d18a3c8dd9466714aa4446f8eab1
Tomasz Kaczmarz <tomasz@beamable.com>
2022-10-04 14:54:34 +0200

    Document methods

--- Assets/Beamable/Microservices/EthereumExample/EthereumExample.cs
+++ Assets/Beamable/Microservices/EthereumExample/EthereumExample.cs
@@ -8,22 +8,36 @@ namespace Beamable.Microservices
 	[Microservice("EthereumExample")]
 	public class EthereumExample : Microservice
 	{
+		/// <summary>
+		/// It's used to get a wallet address linked to the given private key.
+		/// </summary>
+		/// <param name="msg">A message you want to send</param>
+		/// <param name="signature">A signature made using the private key through <see cref="EthereumMessageSigner"/></param>
+		/// <returns>A wallet address</returns>
 		private string GetWalletAddress(string msg, string signature)
 		{
 			var signer1 = new EthereumMessageSigner();
 			return signer1.EncodeUTF8AndEcRecover(msg, signature);
 		}
 		
+		/// <summary>
+		/// Associate wallet address with a player using Stats.
+		/// </summary>
+		/// <param name="msg1">A message you want to send</param>
+		/// <param name="signature1">A signature made using the private key through <see cref="EthereumMessageSigner"/></param>
+		/// <returns>Resolved wallet address</returns>
 		[ClientCallable]
 		public async Task<string> AssociateWallet(string msg1, string signature1)
 		{
 			// This code executes on the server.
 			string walletAddress = GetWalletAddress(msg1, signature1);
-			var stats = new Dictionary<string, string>()
+			// prepare a dictionary with a wallet address stat
+			var stats = new Dictionary<string, string>
 			{
 				{"walletAddr", walletAddress}
 			};
 
+			// write wallet address stat to the backend
 			await Services.Stats.SetStats(
 				"game", 
 				"private", 
--- Assets/EthereumTest.cs
+++ Assets/EthereumTest.cs
@@ -11,12 +11,14 @@ public class EthereumTest : MonoBehaviour
     public string address;
     public string privateKey;
     
-    async void Start()
+    private async void Start()
     {
         var msg1 = "wee test message 18/09/2017 02:55PM";
         var signer1 = new EthereumMessageSigner();
+        // get wallet's signature using it's private key
         var signature1 = signer1.EncodeUTF8AndSign(msg1, new EthECKey(privateKey));
 
+        // associate wallet address with a player using a microservice
         var client = new EthereumExampleClient();
         var resolvedAddress = await client.AssociateWallet(msg1, signature1);
         
@@ -25,13 +27,19 @@ public class EthereumTest : MonoBehaviour
         StartCoroutine(GetWalletBalance());
     }
 
-    public IEnumerator GetWalletBalance()
+    /// <summary>
+    /// This method sends a request to check the balance of the associated wallet
+    /// </summary>
+    private IEnumerator GetWalletBalance()
     {
+        // create a balance request passing a URL to the Ethereum client
         var balanceRequest = new EthGetBalanceUnityRequest(
             "https://mainnet.infura.io/v3/ab915718ee34490e81e1f62360fee96f");
         
+        // send the request and wait for response
         yield return balanceRequest.SendRequest(address, BlockParameter.CreateLatest());
         
+        // convert the value from requests result
         Debug.Log("Balance of account:" + UnitConversion.Convert.FromWei(balanceRequest.Result.Value));
     }
 }
